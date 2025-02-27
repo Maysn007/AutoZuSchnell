211ce3fca95335632d0ae51ee93df774ca7e1f02
Mathias Junger <mjunger@schueler.hakzell.at>
2025-02-27 07:40:21 +0100

    Projektdateien hinzufügen.

---
+++ Auto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoZuSchnell;
+
+    public class Auto
+    {
+    public string Kennzeichen { get; set; }
+    public int Geschwindigkeit { get; set; }
+    public Auto(string kennzeichen, int geschwindigkeit)
+    {
+        Kennzeichen = kennzeichen;
+        Geschwindigkeit = geschwindigkeit;
+    }
+    public override string ToString()
+    {
+        return $"Kennzeichen: {Kennzeichen}, Geschwindigkeit: {Geschwindigkeit}";
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using AutoZuSchnell;
+
+List<Auto> cars = new List<Auto>()
+{
+    new Auto ("WI1216", 50),
+    new Auto ("ZE7134", 100),
+    new Auto ("ZE7135", 120),
+    new Auto ("ZE7136", 130),
+};
+
+var autozuschnell = from car in cars
+                    where car.Geschwindigkeit > 100
+                    select car;
+
+foreach (Auto auto in autozuschnell)
+{
+    Console.WriteLine(auto);
+}
